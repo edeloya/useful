a54e95d2b3e6f171164463ed816babbccb001c03
edeloya <54195989+edeloya@users.noreply.github.com>
2021-08-12 15:18:54 -0500

    Add files via upload

---
+++ crop.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+SET /p file="Input filename (with extension): "
+
+::FOR loops through "tokens" in the file output, storing them for use with SET
+::you can specify "tokens=3" to ONLY set vartmp to the 3rd "word" from the findstr output
+
+ffmpeg -i "%file%" -t 10 -vf cropdetect -f null - 2> crop.tmp && attrib +h crop.tmp
+FOR /F "useback tokens=14 delims= " %%A IN ("crop.tmp") DO (SET "lastline=%%A")
+ffmpeg -i "%file%" -vf "%lastline%" "%file:~0,-4%_cropped.mkv"
+del crop.tmp
+PAUSE
+
+::more info at:
+::https://ss64.com/nt/for_f.html
+::https://ss64.com/nt/set.html
+::https://ss64.com/nt/syntax-substring.html
---
+++ youtube-clipper.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+SET /p url="Paste YouTube URL: "
+SET /p ss="Start (seconds or timecode eg. 7:28): "
+SET /p t="End (seconds or timecode): "
+SET /p extra="Extra ffmpeg params? (Enter to skip): "
+
+::piping youtube-dl STDOUT to findstr in the (''). Trouble chars in for loop escaped with ^ and with / in youtube-dl and findstr
+::FOR loops through "tokens" in the findstr output, storing them for use with SET
+::you can specify "tokens=3" to ONLY set vartmp to the 3rd "word" from the findstr output
+FOR /f "tokens=*" %%a in ('youtube-dl "%url%" ^| findstr "[\"].*[\"]"') do (SET vartmp=%%a)
+SET coolvid=%vartmp:~30%
+
+::Takes full coolvid and cuts it by time into output.mp4. -y allows file overwrites. End the line with 2>nul to hide ffmpeg text
+ffmpeg -ss %ss% -to %t% -i %coolvid% %extra% output.mp4 -y 2>nul
+
+::delet full video after
+del %coolvid%
+PAUSE
+
+::more info at:
+::https://ss64.com/nt/for_f.html
+::https://ss64.com/nt/set.html
+::https://ss64.com/nt/syntax-substring.html
+
+::useful ffmpeg filters
+::https://ffmpeg.org/ffmpeg-filters.html#Examples-142
+::https://ffmpeg.org/ffmpeg-filters.html#scale-1
+
+::-vf "scale=-2:360,setpts=0.5*PTS" -> -vf uses filters/params in "", delimited by ','
+::scale=h:w, -n to make sure its a multiple of n (-2 tells it to autoscale to an even h at approx natural aspect ratio to 360)
+::setpts=0.5*PTS, change presentation timestamp by multiple. .5*PTS speeds up video by 
