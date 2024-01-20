aa2d97bf5b5b651976375f947ea3179032beb991
edeloya <deloyaeric@gmail.com>
2024-01-20 02:13:11 -0600

    Update fresh10.ps1

--- fresh10.ps1
+++ fresh10.ps1
@@ -7,3 +7,5 @@ Get-WindowsUpdate -AcceptAll -Install -IgnoreReboot
 irm https://community.chocolatey.org/install.ps1 | iex
 New-ItemProperty -Path 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name ShowSecondsInSystemClock -PropertyType dword -Value 1;
 New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name VerboseStatus -PropertyType dword -Value 1;
+New-Item -Path 'HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\';
+New-Item -Path 'HKCU:\SOFTWARE\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32';
