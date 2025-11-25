74efc8c1f077bb63465c981079e7a1fe1717d65b
edeloya <deloyaeric@gmail.com>
2025-11-25 12:12:47 -0600

    Update install-Termux-Whisper.sh

--- install-Termux-Whisper.sh
+++ install-Termux-Whisper.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-
 set -x
 cd ~
-pkg update -y
-pkg upgrade -y
-pkg install -y git cmake clang make ffmpeg curl
 termux-setup-storage
-git clone --depth 1 https://github.com/ggerganov/whisper.cpp.git
-cd whisper.cpp
+pkg update -y && pkg upgrade -y
+pkg install -y git cmake clang make ffmpeg curl
+git clone --depth 1 https://github.com/ggerganov/whisper.cpp.git & cd whisper.cpp
 cmake -B build
 cmake --build build --config Release
 bash ./models/download-ggml-model.sh base.en
 
-cat >> $PREFIX/etc/bash.bashrc << 'EOF'
-
+# Check if transcribe_audio exists
+if ! type transcribe_audio &>/dev/null; then
+    # Append to bash.bashrc if not
+    cat >> "$PREFIX/etc/bash.bashrc" << 'EOF'
 
 # Whisper.cpp
 transcribe_audio() {
@@ -25,19 +24,18 @@ transcribe_audio() {
   input_file="$1"
   input_dir="$(dirname "$input_file")"
   base_name="$(basename "$input_file" | sed 's/\.[^.]*$//')"
-
-  # Default output file name if not provided
   wav_file="$input_dir/${base_name}_temp.wav"
 
   # Convert to WAV in same directory
   ffmpeg -i "$input_file" -f wav "$wav_file"
 
-  # Run Whisper (CLI version)
+  # Run Whisper-cli
   ~/whisper.cpp/build/bin/whisper-cli -m ~/whisper.cpp/models/ggml-base.en.bin -f "$wav_file" -l en -otxt -of "$input_dir/$base_name"
 
   # Clean up temp WAV
   rm -f "$wav_file"
 }
 EOF
+fi
 
 source $PREFIX/etc/bash.bashrc
